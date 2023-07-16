d74821480bb5fa3f20f9f89de6a3529219c2be71
Saw <ehkalanephaung96@gmail.com>
2023-07-16 12:16:05 +0700

    Add code in EventCard.vue and changed let to const in main.ts

---
+++ lab2/src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<!--  HTML or template -->
+<template>
+  <div class="event-class"></div>
+</template>
+
+<!-- typescript -->
+<script setup lang="ts"></script>
+
+<!-- CSS or styles -->
+<style scoped>
+.event-class {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-class:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0, 2);
+}
+</style>
--- lab2/src/main.ts
+++ lab2/src/main.ts
@@ -8,10 +8,11 @@ import router from './router'
 
 const app = createApp(App)
 
+// cannot use let coz strings
+const x = 'helxysx'
+console.log(x)
+
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
-
-let x = 'helxysx'
-console.log(x)
