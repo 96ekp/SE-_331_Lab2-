6c48e99bd71fc699df3c23da69359efbcd3a28e3
Saw <ehkalanephaung96@gmail.com>
2023-07-16 14:57:52 +0700

    5.4 update and add aboutView

--- lab2/src/router/index.ts
+++ lab2/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeListView from '../views/HomeListView.vue'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'EventList',
-      component: HomeListView
+      component: EventListView
     },
     {
       path: '/about',
