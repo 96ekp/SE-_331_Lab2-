44f0a9cd9d04024205884ae5a11754ecadd94a03
Saw <ehkalanephaung96@gmail.com>
2023-07-16 14:50:57 +0700

    5.3 change file name express HomeView to EventListView

--- lab2/src/router/index.ts
+++ lab2/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeListView from '../views/HomeListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'EventList',
+      component: HomeListView
     },
     {
       path: '/about',
