9cd45ef2dccce41745c2590fabe003fa48b5c201
Saw <ehkalanephaung96@gmail.com>
2023-07-28 21:38:50 +0700

    Create EventDetailView to show detail about event

---
+++ lab2/src/views/EventDetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import type { EventItem } from '@/type'
+const event: Ref<EventItem | null> = ref(null)
+</script>
