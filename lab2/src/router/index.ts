7adc0e3b49374807e8beb6da6c83dd5c3e3efa3e
Saw <ehkalanephaung96@gmail.com>
2023-07-29 13:10:57 +0700

    updated query parameter

--- lab2/src/router/index.ts
+++ lab2/src/router/index.ts
@@ -11,8 +11,10 @@ const router = createRouter({
     {
       path: '/',
       name: 'home', // event-list
-      component: EventListView
+      component: EventListView,
+      props: (route) => ({ page: parseInt((route.query?.page as string) || '1') })
     },
+
     {
       path: '/about',
       name: 'about',
