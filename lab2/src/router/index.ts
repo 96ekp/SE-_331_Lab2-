d1e0582234247655e91752cc642b2e2c7821b879
Saw <ehkalanephaung96@gmail.com>
2023-07-29 22:23:49 +0700

    fix 3 events import

--- lab2/src/router/index.ts
+++ lab2/src/router/index.ts
@@ -3,9 +3,9 @@ import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
 import CategoryView from '../views/CategoryView.vue'
 import StudentView from '../views/StudentView.vue'
-import EventDetailView from '../views/event/EventDetailView.vue'
-import EventDetailView from '../views/event/EventEditView.vue'
-import EventDetailView from '../views/event/EventRegisterView.vue'
+import EventDetailView from '../views/event/EventDetailView.vue' // Correct import
+import EventEditView from '../views/event/EventEditView.vue' // Correct import
+import EventRegisterView from '../views/event/EventRegisterView.vue' // Correct import
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
