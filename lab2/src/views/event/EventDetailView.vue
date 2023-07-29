5d07bd7f1302dcaf0e78b0185ef96348bb46a29a
Saw <ehkalanephaung96@gmail.com>
2023-07-29 22:37:54 +0700

    just remove duplicated codes

--- lab2/src/views/event/EventDetailView.vue
+++ lab2/src/views/event/EventDetailView.vue
@@ -1,35 +1,18 @@
 <!-- views/event/EventDetailView.vue -->
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import { type EventItem } from '@/type'
-import EventService from '@/services/EventService'
-
-const event = ref<EventItem | null>(null)
-
-const props = defineProps({
-  id: String
+import type { PropType } from 'vue'
+defineProps({
+  event: {
+    type: Object as PropType<EventItem>,
+    require: true
+  }
 })
-
-EventService.getEventById(Number(props.id))
-  .then((response) => {
-    event.value = response.data
-  })
-  .catch((error) => {
-    console.log(error)
-  })
 </script>
 
 <template>
   <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <div id="nav">
-      <!-- Links to different views related to the event using router-link -->
-      <router-link :to="{ name: 'EventDetails', params: { id } }">Details</router-link> |
-      <router-link :to="{ name: 'EventRegister', params: { id } }">Register</router-link> |
-      <router-link :to="{ name: 'EventEdit', params: { id } }">Edit</router-link>
-    </div>
-
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
   </div>
