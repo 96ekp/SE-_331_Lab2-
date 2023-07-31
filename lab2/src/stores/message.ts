f7b06b9e93074da044752d729d15799b8a864480
Saw <ehkalanephaung96@gmail.com>
2023-07-31 13:18:54 +0700

    add the action of the store for text to be show and remove

--- lab2/src/stores/message.ts
+++ lab2/src/stores/message.ts
@@ -3,5 +3,13 @@ import { defineStore } from 'pinia'
 export const useMessageStore = defineStore('message', {
   state: () => ({
     message: '' as string
-  })
+  }),
+  actions: {
+    updateMessage(message: String) {
+      this.message = message
+    },
+    resetMessage() {
+      this.message = ''
+    }
+  }
 })
