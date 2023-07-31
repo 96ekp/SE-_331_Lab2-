e1e885489ef7a76734a332860dd7bbc44071a886
Saw <ehkalanephaung96@gmail.com>
2023-07-31 13:24:24 +0700

    adding register button

--- lab2/src/views/event/EventRegisterView.vue
+++ lab2/src/views/event/EventRegisterView.vue
@@ -3,12 +3,23 @@
 <script setup lang="ts">
 import { type EventItem } from '@/type'
 import type { PropType } from 'vue'
-defineProps({
+import { useRouter } from 'vue-router'
+const props = defineProps({
   event: {
     type: Object as PropType<EventItem>,
     require: true
   }
 })
+const router = useRouter()
+
+function register() {
+  router.push({
+    name: 'event-detail',
+    params: {
+      id: props.event?.id
+    }
+  })
+}
 </script>
 
 <template>
