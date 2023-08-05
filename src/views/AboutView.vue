2468ecec09f18de6c982cbb74024bdc54ab4fd9e
Saw <ehkalanephaung96@gmail.com>
2023-08-05 15:28:22 +0700

    just change about page style

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="lg:min-h-screen lg:flex lg:items-center">
+  <div class="lg:min-h-screen lg:flex lg:items-center lg:justify-center">
     <h1>This is an about page</h1>
   </div>
 </template>
