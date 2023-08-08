9b78aa2c1fe6a458b8daee9af345e9386eaa138f
Saw <ehkalanephaung96@gmail.com>
2023-08-09 01:48:55 +0700

    fixed bugs

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -7,7 +7,7 @@
   </div>
 </template>
 
-<script setup>
+<script lang="ts">
 import { ref, onMounted } from 'vue'
 import StudentService from '@/services/StudentService'
 import StudentCard from '@/components/StudentCard.vue'
